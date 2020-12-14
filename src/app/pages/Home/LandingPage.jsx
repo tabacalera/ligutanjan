90446516c74fbbc84a07cea917f81011cb002b49
tabacalera <calerataba@gmail.com>
2020-12-14 21:47:53 +0800

    Mobile Friendly

--- src/app/pages/Home/LandingPage.jsx
+++ src/app/pages/Home/LandingPage.jsx
@@ -1,12 +1,16 @@
-import { Row } from "antd";
+import { Col, Row } from "antd";
 import Title from "antd/lib/typography/Title";
 import React from "react";
 
 const LandingPage = (props) => {
   return (
     <Row justify="center" style={{marginTop:"50px"}}>
-      <Title>Jan Michael Ligutan's Portfolio Manager</Title>
-      <img src="/assets/images/coming-soon.jpg" style={{width:"90%"}} alt="Coming Soon"></img>
+      <Col span={22}>
+        <Row gutter={24} justify="center" style={{textAlign:"center"}}>
+          <Title>Jan Michael Ligutan's Portfolio Manager</Title>
+          <img src="/assets/images/coming-soon.jpg" style={{width:"90%"}} alt="Coming Soon"></img>
+        </Row>
+      </Col>
     </Row>
   );
 };
