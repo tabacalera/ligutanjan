b606e12be3a066f02114b14ed4f7d1d8a3e7e244
tabacalera <calerataba@gmail.com>
2020-12-14 21:45:50 +0800

    Added Coming Soon

--- src/app/pages/Home/LandingPage.jsx
+++ src/app/pages/Home/LandingPage.jsx
@@ -1,7 +1,14 @@
+import { Row } from "antd";
+import Title from "antd/lib/typography/Title";
 import React from "react";
 
 const LandingPage = (props) => {
-  return <div>Jan Michael Ligutan's Portfolio Manager. Coming Soon...</div>;
+  return (
+    <Row justify="center" style={{marginTop:"50px"}}>
+      <Title>Jan Michael Ligutan's Portfolio Manager</Title>
+      <img src="/assets/images/coming-soon.jpg" style={{width:"90%"}} alt="Coming Soon"></img>
+    </Row>
+  );
 };
 
 LandingPage.propTypes = {};
